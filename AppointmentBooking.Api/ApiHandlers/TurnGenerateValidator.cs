fd6095321bf57d5d6fb350523b6a7bddad9a89dc
klmeir <caalmeir@gmail.com>
2024-06-12 15:38:25 -0500

    Added custom validation rules

--- AppointmentBooking.Api/ApiHandlers/TurnGenerateValidator.cs
+++ AppointmentBooking.Api/ApiHandlers/TurnGenerateValidator.cs
@@ -5,11 +5,22 @@ namespace AppointmentBooking.Api.ApiHandlers
 {
     public class TurnGenerateValidator : AbstractValidator<TurnGenerateCommand>
     {
+        private string DateFormat { get; set; }
+
         public TurnGenerateValidator()
-        {            
-            RuleFor(x => x.StartDate).NotEmpty();
-            RuleFor(x => x.EndDate).NotEmpty();
-            RuleFor(x => x.IdService).NotEmpty();
+        {
+            DateFormat = "dd/MM/yyyy";
+            RuleFor(x => x.StartDate).NotEmpty().Must(BeValidDate).WithMessage($"Invalid date format. The date should be in format '{DateFormat}'"); ;
+            RuleFor(x => x.EndDate).NotEmpty().Must(BeValidDate).WithMessage($"Invalid date format. The date should be in format '{DateFormat}'"); ;
+            RuleFor(x => x.IdService).NotEmpty().WithMessage("IdService is required");
+        }
+
+        private bool BeValidDate(string date)
+        {
+            if (string.IsNullOrWhiteSpace(date))
+                return true;
+
+            return DateTime.TryParseExact(date, DateFormat, null, System.Globalization.DateTimeStyles.None, out _);
         }
     }
 }
