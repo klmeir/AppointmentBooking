5f5b40107a17068cdeb2c9e91595448a6f493592
klmeir <caalmeir@gmail.com>
2024-06-12 14:04:15 -0500

    Added unit testing project for the domain layer

---
+++ AppointmentBooking.Domain.Tests/GenerateTurnTests.cs
@@ -0,0 +1,50 @@
+using AppointmentBooking.Domain.Entities;
+using AppointmentBooking.Domain.Exception;
+using AppointmentBooking.Domain.Ports;
+using AppointmentBooking.Domain.Services;
+using NSubstitute;
+
+namespace AppointmentBooking.Domain.Tests
+{
+    public class GenerateTurnTests
+    {
+        readonly ITurnRepository _repository = default!;        
+        readonly GenerateTurnService _service = default!;
+
+        public GenerateTurnTests()
+        {            
+            _repository = Substitute.For<ITurnRepository>();
+            _service = new GenerateTurnService(_repository);
+        }        
+
+        [Fact]
+        public async void GenerateTurns_WithWrongStartDate()
+        {
+            try
+            {
+                TurnGenerate turnGenerate = new(DateOnly.FromDateTime(DateTime.Now.AddDays(-1)), DateOnly.FromDateTime(DateTime.Now), 1);
+                await _service.GenerateTurns(turnGenerate);
+                Assert.Fail("It shouldn't get to this point");
+            }
+            catch (CoreBusinessException cbe)
+            {
+                Assert.True(cbe.Message.Equals("Start date must be equal to or greater than the current date"));
+            }
+        }
+
+        [Fact]
+        public async void GenerateTurns_WithWrongEndDate()
+        {
+            try
+            {
+                TurnGenerate turnGenerate = new(DateOnly.FromDateTime(DateTime.Now), DateOnly.FromDateTime(DateTime.Now.AddDays(-1)), 1);
+                await _service.GenerateTurns(turnGenerate);
+                Assert.Fail("It shouldn't get to this point");
+            }
+            catch (CoreBusinessException cbe)
+            {
+                Assert.True(cbe.Message.Equals("End date must be equal to or greater than the start date"));
+            }
+        }
+    }
+}
